f809133166f319ad3d980955efb60153cc1b44aa
Alexander Cyon <alex@radixdlt.com>
2019-03-11 13:55:30 +0100

    Adding Xcodeproject file

---
+++ Sources/SupportingFiles/StickyEncoding.h
@@ -0,0 +1,19 @@
+//
+//  StickyEncoding.h
+//  StickyEncoding
+//
+//  Created by Alexander Cyon on 2019-03-11.
+//  Copyright © 2019 Radix DLT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickyEncoding.
+FOUNDATION_EXPORT double StickyEncodingVersionNumber;
+
+//! Project version string for StickyEncoding.
+FOUNDATION_EXPORT const unsigned char StickyEncodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickyEncoding/PublicHeader.h>
+
+
